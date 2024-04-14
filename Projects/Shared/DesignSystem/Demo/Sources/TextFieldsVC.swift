da2754e786bc280f83e654dd50c4b929eccf082d
malrang <ruehd7924@gmail.com>
2024-04-14 18:29:12 +0900

    feat: TextArea 추가및 구현

--- Projects/Shared/DesignSystem/Demo/Sources/TextFieldsVC.swift
+++ Projects/Shared/DesignSystem/Demo/Sources/TextFieldsVC.swift
@@ -6,6 +6,8 @@ final class TextFieldsVC: UIViewController {
 
   private let input = Input(charactorLimitCount: 20, title: "Input", placeholeder: "플레이스 홀더")
 
+  private let textArea = TextArea(charactorLimitCount: 20, title: "TextArea", placeholeder: "플레이스 홀더")
+
   private let rootContainer = UIView()
 
   init() {
@@ -25,8 +27,12 @@ final class TextFieldsVC: UIViewController {
 
   private func setupConstraints() {
     view.addSubview(rootContainer)
-    rootContainer.flex.justifyContent(.center).define { flex in
-      flex.addItem(input).marginHorizontal(20)
+    rootContainer.flex.justifyContent(.center).paddingHorizontal(20).define { flex in
+      flex.addItem(input)
+
+      flex.addItem().height(20)
+
+      flex.addItem(textArea)
     }
   }
 
