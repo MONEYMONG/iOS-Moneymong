607f51589bba0ca9e5cdd321edcf8a2470c55954
Safari <111dltldnjs@gmail.com>
2024-04-20 20:00:01 +0900

    feat: LedgerVC 장부, 멤버 tab 추가 & Demo

--- Projects/Feature/Ledger/Sources/Ledger/LedgerVC.swift
+++ Projects/Feature/Ledger/Sources/Ledger/LedgerVC.swift
@@ -4,11 +4,37 @@ import BaseFeature
 import ReactorKit
 import PinLayout
 import FlexLayout
+import DesignSystem
 
 public final class LedgerVC: BaseVC, View {
   public var disposeBag = DisposeBag()
-  weak var coordinator: LedgerCoordinator?
-
+  weak var coordinator: Coordinator?
+  private let childVC: [UIViewController]
+  
+  private lazy var lineTab = LineTabViewController(childVC)
+  
+  init(_ childVC: [UIViewController]) {
+    self.childVC = childVC
+    super.init()
+  }
+  
+  public override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    rootContainer.pin.all(view.safeAreaInsets)
+  }
+  
+  public override func setupUI() {
+    super.setupUI()
+  }
+  
+  public override func setupConstraints() {
+    super.setupConstraints()
+    rootContainer.flex
+      .define { flex in
+        flex.addItem(lineTab.view)
+      }
+  }
+  
   public func bind(reactor: LedgerReactor) {
     
   }
--- Projects/Feature/Ledger/Sources/LedgerDIContainer.swift
+++ Projects/Feature/Ledger/Sources/LedgerDIContainer.swift
@@ -1,10 +1,42 @@
+import UIKit
+
+import BaseFeature
+
 public final class LedgerDIContainer {
   public init() {}
-
-  func ledger(with coordinator: LedgerCoordinator) -> LedgerVC {
-    let vc = LedgerVC()
+  
+  func ledger(with coordinator: Coordinator) -> LedgerVC {
+    let vc = LedgerVC(
+      [
+        childLeger(with: coordinator),
+        childMember(with: coordinator)
+      ]
+    )
     vc.reactor = LedgerReactor()
     vc.coordinator = coordinator
+    return vc
+  }
+  
+  private func childLeger(with coordinator: Coordinator) -> UIViewController {
+    let vc = UINavigationController()
+    let childLedgerCoordinator = ChildLedgerCoordinator(
+      navigationController: vc,
+      diContainer: ChildLedgerDIContainer()
+    )
+    coordinator.childCoordinators.append(childLedgerCoordinator)
+    childLedgerCoordinator.start(animated: false)
+    return vc
+  }
+  
+  private func childMember(with coordinator: Coordinator) -> UIViewController {
+    let vc = UINavigationController()
+    let childMemberCoordinator = ChildMemberCoordinator(
+      navigationController: vc,
+      diContainer: ChildMemberDIContainer()
+    )
+    coordinator.childCoordinators.append(childMemberCoordinator)
+    childMemberCoordinator.parentCoordinator = coordinator
+    childMemberCoordinator.start(animated: false)
     return vc
   }
 }
