199a901a8cc6ad088dd0e8b040ff93981ff224ed
Siwon-L <111dltldnjs@gmail.com>
2024-12-29 12:24:35 +0900

    feat: Ledger Domain 생성

--- Projects/Domain/Ledger/Project.swift
+++ Projects/Domain/Ledger/Project.swift
@@ -1,55 +1,56 @@
 import ProjectDescription
 
 let project = Project(
-    name: "Auth",
+    name: "Ledger",
     settings: .settings(
         base: .init()
         .swiftVersion("5.7")
     ),
     targets: [
         Target(
-            name: "Auth",
+            name: "Ledger",
             platform: .iOS,
             product: .framework,
-            bundleId: "com.framework.moneymong.Auth",
+            bundleId: "com.framework.moneymong.Ledger",
             deploymentTarget: .iOS(targetVersion: "15.0", devices: .iphone),
             sources: ["Sources/**"],
             dependencies: [
-                .target(name: "AuthInterface"),
+                .target(name: "LedgerInterface"),
                 .project(target: "Core", path: .relativeToRoot("Projects/Core/Core"))
             ]
         ),
         Target(
-            name: "AuthInterface",
+            name: "LedgerInterface",
             platform: .iOS,
             product: .framework,
-            bundleId: "com.framework.moneymong.AuthInterface",
+            bundleId: "com.framework.moneymong.LedgerInterface",
             deploymentTarget: .iOS(targetVersion: "15.0", devices: .iphone),
             sources: ["Interface/**"],
             dependencies: [
             ]
         ),
         Target(
-            name: "AuthTests",
+            name: "LedgerTests",
             platform: .iOS,
             product: .unitTests,
-            bundleId: "com.framework.moneymong.AuthTests",
+            bundleId: "com.framework.moneymong.LedgerTests",
             deploymentTarget: .iOS(targetVersion: "15.0", devices: .iphone),
             sources: ["Tests/**"],
             dependencies: [
-                .target(name: "Auth"),
-                .target(name: "AuthTesting")
+                .target(name: "Ledger"),
+                .target(name: "LedgerTesting"),
+                .project(target: "CoreTesting", path: .relativeToRoot("Projects/Core/Core"))
             ]
         ),
         Target(
-            name: "AuthTesting",
+            name: "LedgerTesting",
             platform: .iOS,
             product: .staticLibrary,
-            bundleId: "com.framework.moneymong.AuthTesting",
+            bundleId: "com.framework.moneymong.LedgerTesting",
             deploymentTarget: .iOS(targetVersion: "15.0", devices: .iphone),
             sources: ["Testing/**"],
             dependencies: [
-                .target(name: "AuthInterface")
+                .target(name: "LedgerInterface")
             ]
         )
     ]
---
+++ Projects/Domain/Ledger/Tests/LedgerTests.swift
@@ -0,0 +1,13 @@
+import XCTest
+@testable import Ledger
+
+final class LedgerTests: XCTestCase {
+
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws {}
+
+    func testExample() throws {
+        XCTAssertEqual(1, 1)
+    }
+}
