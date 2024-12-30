12ed1890563d148a73bf4e83e33a0f6f9595c8c9
Siwon-L <111dltldnjs@gmail.com>
2024-12-30 10:49:25 +0900

    feat: UploadDocumentUseCase 구현

---
+++ Projects/Domain/Ledger/Sources/UploadDocumentUseCase.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+import Core
+import LedgerInterface
+
+public struct UploadDocumentUseCase: UploadDocumentUseCaseInterface {
+  private let ledgerRepo: LedgerRepositoryInterface
+  
+  public init(ledgerRepo: LedgerRepositoryInterface) {
+    self.ledgerRepo = ledgerRepo
+  }
+  
+  public func execute(ledgerID: Int, documentUrls: [String]) async throws {
+    try await ledgerRepo.documentImagesUpload(detailId: ledgerID, documentImageUrls: documentUrls)
+  }
+}
