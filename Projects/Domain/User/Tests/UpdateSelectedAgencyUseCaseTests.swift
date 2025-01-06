1609d6a39309127f06cf664e4a4ffb805af63252
Siwon-L <111dltldnjs@gmail.com>
2025-01-06 16:37:34 +0900

    feat: UpdateSelectedAgencyUseCase 구현

---
+++ Projects/Domain/User/Tests/UpdateSelectedAgencyUseCaseTests.swift
@@ -0,0 +1,33 @@
+import XCTest
+@testable import CoreTesting
+@testable import Core
+@testable import User
+@testable import UserInterface
+
+final class UpdateSelectedAgencyUseCaseTests: XCTestCase {
+  var sut: UpdateSelectedAgencyUseCase!
+  var mockNetwork: MockNetworkManager!
+  var mockLocalStorage: MockLocalStorage!
+  override func setUpWithError() throws {
+    mockNetwork = MockNetworkManager()
+    mockLocalStorage = MockLocalStorage()
+    let userRepo = UserRepository(networkManager: mockNetwork, localStorage: mockLocalStorage)
+    sut = UpdateSelectedAgencyUseCase(userRepo: userRepo)
+  }
+  
+  override func tearDownWithError() throws {
+    mockNetwork = nil
+    mockLocalStorage = nil
+    sut = nil
+  }
+  
+  func test_execute_호출_시_전달한_값이_LocalStorage에_저장되어야_한다() async {
+    // Arrange
+    let input = 0
+    
+    // Act
+    sut.execute(id: input)
+    // Assert
+    XCTAssertEqual(mockLocalStorage.selectedAgency, input)
+  }
+}
