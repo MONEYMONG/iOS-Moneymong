02cba5f2cc329c9e9b6e38a8083892f7af503c70
Siwon-L <111dltldnjs@gmail.com>
2025-01-06 17:09:45 +0900

    feat: GetUserIDUseCase 구현

---
+++ Projects/Domain/User/Sources/GetUserIDUseCase.swift
@@ -0,0 +1,14 @@
+import Core
+import UserInterface
+
+public struct GetUserIDUseCase: GetUserIDUseCaseInterface {
+  private let userRepo: UserRepositoryInterface
+  
+  public init(userRepo: UserRepositoryInterface) {
+    self.userRepo = userRepo
+  }
+  
+  public func execute() -> Int {
+    userRepo.fetchUserID()
+  }
+}
