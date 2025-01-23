ca243afdd7f84455596c98c06cf28df8c755091a
doyeonKim <whqtkf12@naver.com>
2025-01-23 20:45:55 +0900

    refactor: ConfirmCertificateCodeUseCase를 좀 더 통합적인 케이스로 수정

--- Projects/Domain/Agency/Sources/ConfirmCertificateCodeUseCase.swift
+++ Projects/Domain/Agency/Sources/ConfirmCertificateCodeUseCase.swift
@@ -4,13 +4,24 @@ import Core
 import AgencyInterface
 
 public struct ConfirmCertificateCodeUseCase: ConfirmCertificateCodeUseCaseInterface {
-  private let repo: AgencyRepositoryInterface
+  private let agencyRepo: AgencyRepositoryInterface
+  private let userRepo: UserRepositoryInterface
   
-  public init(repo: AgencyRepositoryInterface) {
-    self.repo = repo
+  public init(
+    agencyRepo: AgencyRepositoryInterface,
+    userRepo: UserRepositoryInterface
+  ) {
+    self.agencyRepo = agencyRepo
+    self.userRepo = userRepo
   }
   
-  public func execute(id: Int, code: String) async throws -> Bool {
-    try await repo.certificateCode(id: id, code: code)
+  public func execute(id: Int, code: [String]) async throws -> Bool {
+    let codes = code.compactMap { $0 }.map { String($0) }.joined()
+    
+    let result = try await agencyRepo.certificateCode(id: id, code: codes)
+    if result {
+      userRepo.updateSelectedAgency(id: id)
+    }
+    return result
   }
 }
