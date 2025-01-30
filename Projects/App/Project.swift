e0761058716fba39412232677bfa6d4ecd37c5b6
Siwon-L <111dltldnjs@gmail.com>
2025-01-30 14:39:35 +0900

    chore: version 1.4.1 -> 1.4.2

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -8,7 +8,7 @@ let project = Project(
   ),
   settings: .settings(
     base: .init()
-      .marketingVersion("1.4.1")
+      .marketingVersion("1.4.2")
       .swiftVersion("5.7")
       .currentProjectVersion("1")
       .appleGenericVersioningSystem(),
