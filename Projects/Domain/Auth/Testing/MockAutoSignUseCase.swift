9d8f9f8154582cd654b297e15a73cd953f1ff85f
Siwon-L <111dltldnjs@gmail.com>
2025-05-06 13:26:32 +0900

    fix: Sign Demo App 수정
    - AutoSign, GetRecentLoginInfo, SignUp Mock UseCase 추가
    - SignFeatureDemo -> BaseFeatureTesting 모듈 의존성 추가

---
+++ Projects/Domain/Auth/Testing/MockAutoSignUseCase.swift
@@ -0,0 +1,9 @@
+import AuthInterface
+import Utility
+public struct MockAutoSignUseCase: AutoSignUseCaseInterface {
+  public init() {}
+  
+  public func execute() async throws {
+    throw MoneyMongError.appError(.default, errorMessage: "")
+  }
+}
