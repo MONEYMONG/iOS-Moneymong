f58c4cda7dfd43301cbd7f70a4ec7cb8765420d2
Siwon-L <111dltldnjs@gmail.com>
2025-12-11 15:59:19 +0900

    refactor: add weakTable to DIContainer

--- Projects/Feature/Base/Sources/Common/DIContainer.swift
+++ Projects/Feature/Base/Sources/Common/DIContainer.swift
@@ -1,21 +1,40 @@
 import Foundation
 
 public final class DIContainer {
+  private struct WeakElement {
+    weak var element: AnyObject?
+    
+    init(element: AnyObject? = nil) {
+      self.element = element
+    }
+  }
   
   public static let shared: DIContainer = .init()
   
   private init() {}
   
   private var storage: [String : () -> Any] = [:]
+  private var weakTable: [String : WeakElement] = [:]
   
   public func register<T>(type: T.Type, value: @escaping () -> T) {
     storage[String(describing: type)] = value
   }
   
   public func resolve<T>(type: T.Type) -> T {
-    guard let object = storage[String(describing: type)]?() as? T else {
+    let key = String(describing: type)
+    
+    if let element = weakTable[key]?.element as? T {
+        return element
+    }
+    
+    guard let object = storage[key]?() as? T else {
       fatalError("Could not resolve \(type)")
     }
+    
+    if Mirror(reflecting: object).displayStyle == .class {
+      weakTable[key] = WeakElement(element: object as AnyObject)
+    }
+    
     return object
   }
 }
